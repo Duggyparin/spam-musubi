6a0a52c5d61790c94ed43f67506938de9d66502a
Duggyparin <monsanto.bryann@gmail.com>
2026-04-05 22:16:53 +0800

    Fix API key – copy from Firebase Console

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,16 +6,17 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPLyRkA9qib7YdeyrEz5R6FguPOe6i7cA",
-  authDomain: "spam-musubi-aleab.firebaseapp.com",
-  projectId: "spam-musubi-aleab",
-  storageBucket: "spam-musubi-aleab.firebasestorage.app",
+  apiKey: "AIzaSyCPLyRka9qib7YdeyrEz5R6FguPOe6i7cA",
+  authDomain: "spam-musubi-a1eab.firebaseapp.com",
+  projectId: "spam-musubi-a1eab",
+  storageBucket: "spam-musubi-a1eab.firebasestorage.app",
   messagingSenderId: "74371417008",
   appId: "1:74371417008:web:338ddfe4618a4a6cdacc75",
   measurementId: "G-C1KCWT9H5R"
 };
 
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
