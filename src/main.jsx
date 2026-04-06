acbf4675924f6592eb8481111aa65f9661f4d80a
Duggyparin <monsanto.bryann@gmail.com>
2026-04-06 18:31:09 +0800

    Switch to HashRouter to fix mobile login iframe conflict

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'   // ← changed from BrowserRouter
 import './index.css'
 import App from './App.jsx'
 
@@ -8,8 +8,8 @@ console.log('main.jsx executing')
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
